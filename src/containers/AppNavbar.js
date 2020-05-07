30e9b3769d15cdb9ab75858c14b644f903051cea
Restyled.io <commits@restyled.io>
2020-05-07 17:28:40 +0000

    Restyled by prettier

--- src/containers/AppNavbar.js
+++ src/containers/AppNavbar.js
@@ -1,14 +1,14 @@
-import styled, {css, up} from "@xstyled/styled-components";
-import {motion} from "framer-motion";
-import {Link} from "gatsby";
+import styled, { css, up } from "@xstyled/styled-components";
+import { motion } from "framer-motion";
+import { Link } from "gatsby";
 import React from "react";
 
-import {Navbar, NavbarBrand, NavbarSecondary} from "../components/Navbar";
+import { Navbar, NavbarBrand, NavbarSecondary } from "../components/Navbar";
 
 const links = {
-  about : "$whoami",
-  blog : "Blog",
-  uses : "Uses",
+  about: "$whoami",
+  blog: "Blog",
+  uses: "Uses",
   // skills: "Skills",
 };
 const LinkStyle = styled.p`
@@ -25,46 +25,46 @@ const LinkStyle = styled.p`
     outline: none;
   }
 
-  ${up("md", css`
+  ${up(
+    "md",
+    css`
       margin: 0 3;
       font-size: 18;
-    `)}
+    `
+  )}
 `;
 
 const list = {
-  visible : {
-    opacity : 1,
-    transition : {
-      when : "beforeChildren",
-      staggerChildren : 0.3,
+  visible: {
+    opacity: 1,
+    transition: {
+      when: "beforeChildren",
+      staggerChildren: 0.3,
     },
   },
-  hidden : {
-    opacity : 0,
-    transition : {
-      when : "afterChildren",
+  hidden: {
+    opacity: 0,
+    transition: {
+      when: "afterChildren",
     },
   },
 };
 
 const item = {
-  visible : {opacity : 1, x : 0},
-  hidden : {opacity : 0, x : -460},
+  visible: { opacity: 1, x: 0 },
+  hidden: { opacity: 0, x: -460 },
 };
 export function AppNavbar() {
   return (
     <motion.div initial="hidden" animate="visible" variants={list}>
       <Navbar>
         {" "}
-        <motion.span
-  whileHover =
-  {
-    { scale: 1.1 }
-  } whileTap =
-      {{ scale: 0.9, }} > <Link to = "/">
-      <NavbarBrand>Piyush Mehta</NavbarBrand>
+        <motion.span whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+          {" "}
+          <Link to="/">
+            <NavbarBrand>Piyush Mehta</NavbarBrand>
           </Link>
-      </motion.span>
+        </motion.span>
         <NavbarSecondary>
           <motion.div
             whileHover={{ scale: 1.1 }}
@@ -72,21 +72,24 @@ export function AppNavbar() {
             variants={item}
           >
             <LinkStyle>
-              <Link to="/about
-       ">{links.about}</Link>
-       < /LinkStyle>
-          </motion.div>< motion.div
-  whileHover =
-  {
-    { scale: 1.1 }
-  } whileTap =
-  {
-    { scale: 0.9 }
-  } variants =
-      {item} > {" "}<LinkStyle>
-      <Link to = "/blog">{links.blog}</Link>
-            </LinkStyle>{" "} <
-      /motion.div>
+              <Link
+                to="/about
+       "
+              >
+                {links.about}
+              </Link>
+            </LinkStyle>
+          </motion.div>
+          <motion.div
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            variants={item}
+          >
+            {" "}
+            <LinkStyle>
+              <Link to="/blog">{links.blog}</Link>
+            </LinkStyle>{" "}
+          </motion.div>
           <motion.div
             whileHover={{ scale: 1.1 }}
             whileTap={{ scale: 0.9 }}
@@ -96,9 +99,12 @@ export function AppNavbar() {
               <Link
                 to="/uses
   "
-      > {links.uses}</Link>
-            </LinkStyle><
-      /motion.div>
+              >
+                {" "}
+                {links.uses}
+              </Link>
+            </LinkStyle>
+          </motion.div>
           <motion.div
             whileHover={{ scale: 1.1 }}
             whileTap={{ scale: 0.9 }}
@@ -109,23 +115,25 @@ export function AppNavbar() {
               <a
                 href="https:/ /
           sourcerer.io / piyush97 "
-  target = "_blank"
-  rel = "noopener noreferrer" > {" "} Portfolio</a>
-            </LinkStyle><
-        motion.div
-  whileHover =
-  {
-    { scale: 1.1 }
-  } whileTap =
-  {
-    { scale: 0.9 }
-  } variants = {item} >
-               <LinkStyle><Link to = "/skills">{
-                   links.skills}</Link>
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {" "}
+                Portfolio
+              </a>
+            </LinkStyle>
+            <motion.div
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+              variants={item}
+            >
+              <LinkStyle>
+                <Link to="/skills">{links.skills}</Link>
               </LinkStyle>
-               </motion.div>
+            </motion.div>
           </motion.div>
-               </NavbarSecondary>
-      </Navbar>< /motion.div>
+        </NavbarSecondary>
+      </Navbar>
+    </motion.div>
   );
 }
