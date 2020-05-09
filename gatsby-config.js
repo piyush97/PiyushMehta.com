2e345523527588a647b7c972292e59f46f32310e
piyush97 <me@piyushmehta.com>
2020-05-09 22:15:49 +0530

    Contentful working

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,13 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+    {
       resolve: 'gatsby-plugin-robots-txt',
       options: {
         host: 'https://www.piyushmehta.com',
