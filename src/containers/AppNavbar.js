64295f908eb3019de9303cd6c3f80bd24dfcf418
Restyled.io <commits@restyled.io>
2020-05-09 13:31:26 +0000

    Restyled by prettier

--- src/containers/AppNavbar.js
+++ src/containers/AppNavbar.js
@@ -1,5 +1,5 @@
-import {motion} from 'framer-motion';
-import {graphql, Link, useStaticQuery} from 'gatsby';
+import { motion } from 'framer-motion';
+import { graphql, Link, useStaticQuery } from 'gatsby';
 import React from 'react';
 
 import {
@@ -10,31 +10,31 @@ import {
 } from '../components/Navbar';
 
 const links = {
-  about : '$whoami',
-  blog : 'Blog',
-  uses : 'Uses',
+  about: '$whoami',
+  blog: 'Blog',
+  uses: 'Uses',
   // skills: "Skills",
 };
 
 const list = {
-  visible : {
-    opacity : 1,
-    transition : {
-      when : 'beforeChildren',
-      staggerChildren : 0.3,
+  visible: {
+    opacity: 1,
+    transition: {
+      when: 'beforeChildren',
+      staggerChildren: 0.3,
     },
   },
-  hidden : {
-    opacity : 0,
-    transition : {
-      when : 'afterChildren',
+  hidden: {
+    opacity: 0,
+    transition: {
+      when: 'afterChildren',
     },
   },
 };
 
 const item = {
-  visible : {opacity : 1, x : 0},
-  hidden : {opacity : 0, x : -460},
+  visible: { opacity: 1, x: 0 },
+  hidden: { opacity: 0, x: -460 },
 };
 
 export function AppNavbar() {
@@ -50,16 +50,11 @@ export function AppNavbar() {
   return (
     <motion.div initial="hidden" animate="visible" variants={list}>
       <Navbar>
-        <motion.span
-  whileHover =
-  {
-    { scale: 1.1 }
-  } whileTap =
-      {{ scale: 0.9, }} >
-      <Link to = "/"><NavbarBrand>{
-          data.site.siteMetadata.title}</NavbarBrand>
-          </Link><
-      /motion.span>
+        <motion.span whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+          <Link to="/">
+            <NavbarBrand>{data.site.siteMetadata.title}</NavbarBrand>
+          </Link>
+        </motion.span>
         <NavbarSecondary>
           <motion.span
             whileHover={{ scale: 1.1 }}
@@ -70,9 +65,12 @@ export function AppNavbar() {
               <Link
                 to="/about
   "
-      > {' '} {links.about}</Link>
-            </NavbarLink><
-      /motion.span>
+              >
+                {' '}
+                {links.about}
+              </Link>
+            </NavbarLink>
+          </motion.span>
           <motion.span
             whileHover={{ scale: 1.1 }}
             whileTap={{ scale: 0.9 }}
@@ -83,9 +81,12 @@ export function AppNavbar() {
               <Link
                 to="/blog
   "
-      > {links.blog}</Link>
-            </NavbarLink>{' '} <
-      /motion.span>
+              >
+                {' '}
+                {links.blog}
+              </Link>
+            </NavbarLink>{' '}
+          </motion.span>
           <motion.span
             whileHover={{ scale: 1.1 }}
             whileTap={{ scale: 0.9 }}
@@ -96,9 +97,12 @@ export function AppNavbar() {
               <Link
                 to="/uses
   "
-      > {' '} {links.uses}</Link>
-            </NavbarLink><
-      /motion.span>
+              >
+                {' '}
+                {links.uses}
+              </Link>
+            </NavbarLink>
+          </motion.span>
           <motion.span
             whileHover={{ scale: 1.1 }}
             whileTap={{ scale: 0.9 }}
@@ -109,7 +113,7 @@ export function AppNavbar() {
               <a
                 href="https:/ /
           sourcerer.io / piyush97 "
-  target = "_blank"
+                target="_blank"
                 rel="noopener noreferrer"
               >
                 Portfolio
