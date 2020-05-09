dd2e46f1ad7339f6b1e4f05ecf14d964419faabb
Restyled.io <commits@restyled.io>
2020-05-09 13:35:07 +0000

    Restyled by clang-format

--- src/containers/AppFooter.js
+++ src/containers/AppFooter.js
@@ -1,10 +1,10 @@
 import styled from '@xstyled/styled-components';
-import { motion } from 'framer-motion';
-import { graphql, useStaticQuery } from 'gatsby';
+import {motion} from 'framer-motion';
+import {graphql, useStaticQuery} from 'gatsby';
 import React from 'react';
-import { FaEnvelope, FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa';
+import {FaEnvelope, FaGithub, FaLinkedin, FaTwitter} from 'react-icons/fa';
 
-import { Container } from '../components/Container';
+import {Container} from '../components/Container';
 
 const Copyright = styled.div`
   color: light100;
@@ -36,10 +36,10 @@ const SocialLink = styled.a`
 const currentYear = new Date().getFullYear();
 
 const locales = {
-  twitter: 'My Twitter profile',
-  github: 'My GitHub profile',
-  linkedin: 'My LinkedIn profile',
-  email: 'Send me an email',
+  twitter : 'My Twitter profile',
+  github : 'My GitHub profile',
+  linkedin : 'My LinkedIn profile',
+  email : 'Send me an email',
 };
 
 export function AppFooter() {
@@ -55,49 +55,38 @@ export function AppFooter() {
   return (
     <Container display="flex" alignItems="center" mt={4} pb={4}>
       <Copyright>
-        {data.site.siteMetadata.title} © {currentYear}
+        {
+    data.site.siteMetadata.title} © {currentYear}
       </Copyright>
       <Socials>
         <motion.span whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
           <SocialLink
             title={locales.twitter}
             href="https://twitter.com/coderWhoKnows"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            {' '}
-            <FaTwitter />
-          </SocialLink>
+  rel = "noopener noreferrer"
+  target = "_blank" > {' '}<FaTwitter /></SocialLink>
         </motion.span>
-        <motion.span whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
-          <SocialLink
-            title={locales.github}
-            href="https://github.com/piyush97"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            {' '}
-            <FaGithub />
-          </SocialLink>
+           <motion.span whileHover = {
+             { scale: 1.1 }
+           } whileTap = {{ scale: 0.9 }}>< SocialLink
+  title = {locales.github} href = "https://github.com/piyush97"
+  rel = "noopener noreferrer"
+  target = "_blank" > {' '}<FaGithub /></SocialLink>
         </motion.span>
 
-        <motion.span whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
-          <SocialLink
-            title={locales.linkedin}
-            href="https://www.linkedin.com/in/piyush24"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            {' '}
-            <FaLinkedin />
-          </SocialLink>
+           <motion.span whileHover = {
+             { scale: 1.1 }
+           } whileTap = {{ scale: 0.9 }}>< SocialLink
+  title = {locales.linkedin} href = "https://www.linkedin.com/in/piyush24"
+  rel = "noopener noreferrer"
+  target = "_blank" > {' '}<FaLinkedin /></SocialLink>
         </motion.span>
 
-        <motion.span whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
-          <SocialLink
-            title={locales.email}
-            href="mailto:me@piyushmehta.com"
-            rel="noopener noreferrer"
+           <motion.span whileHover = {
+             { scale: 1.1 }
+           } whileTap = {{ scale: 0.9 }}>< SocialLink
+  title = {locales.email} href = "mailto:me@piyushmehta.com"
+  rel = "noopener noreferrer"
             target="_blank"
           >
             <FaEnvelope />
