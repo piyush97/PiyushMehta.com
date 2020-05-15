5f158e82ddd3da19abff1ef438c558fa3a53c7b5
piyush97 <me@piyushmehta.com>
2020-05-15 22:01:53 +0530

    sentry init

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,33 +6,52 @@ import { Hero, HeroIntro, HeroTitle, HeroTeaser } from '../containers/Hero';
 import { LatestArticles } from '../containers/LatestArticles';
 import { Seo } from '../containers/Seo';
 
-export default function IndexPage({ data }) {
-  useEffect(() => {
+export default class IndexPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { error: null };
+  }
+  componentDidCatch(error, errorInfo) {
+    this.setState({ error });
+    Sentry.configureScope((scope) => {
+      Object.keys(errorInfo).forEach((key) => {
+        scope.setExtra(key, errorInfo[key]);
+      });
+    });
+    Sentry.captureException(error);
+  }
+  componentDidMount() {
     document.oncontextmenu = function () {
       return false;
     };
-  });
-  return (
-    <>
-      <Hero>
-        <Seo />
-        <HeroIntro>Hi, my name is</HeroIntro>
-        <HeroTitle>
-          <strong>Piyush Mehta</strong>
-          <br />I make the web great.
-        </HeroTitle>
-        <HeroTeaser>
-          I am a Full Stack Developer/Designer based in India. I create, Design
-          tools for web and teach how to build high quality websites and
-          applications using JavaScript, React
-        </HeroTeaser>
-      </Hero>
-      <Container forwardedAs="section" pb={5}>
-        <SectionTitle forwardedAs="h2">Blog</SectionTitle>
-        <LatestArticles edges={data.allMdx.edges} />
-      </Container>
-    </>
-  );
+  }
+  render() {
+    if (this.state.error) {
+      return <h1>Something went wrong!</h1>;
+    } else {
+      return (
+        <>
+          <Hero>
+            <Seo />
+            <HeroIntro>Hi, my name is</HeroIntro>
+            <HeroTitle>
+              <strong>Piyush Mehta</strong>
+              <br />I make the web great.
+            </HeroTitle>
+            <HeroTeaser>
+              I am a Full Stack Developer/Designer based in India. I create,
+              Design tools for web and teach how to build high quality websites
+              and applications using JavaScript, React
+            </HeroTeaser>
+          </Hero>
+          <Container forwardedAs="section" pb={5}>
+            <SectionTitle forwardedAs="h2">Blog</SectionTitle>
+            <LatestArticles edges={this.props.data.allMdx.edges} />
+          </Container>
+        </>
+      );
+    }
+  }
 }
 
 export const pageQuery = graphql`
