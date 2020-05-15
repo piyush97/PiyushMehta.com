ab8cbcda372c52a424d46cb25adf0ddf658719cb
piyush97 <me@piyushmehta.com>
2020-05-15 21:29:06 +0530

    sentry with incomplete blog

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { graphql } from 'gatsby';
+import { graphql, useStaticQuery } from 'gatsby';
 import { SectionTitle } from '../components/Section';
 import { Container } from '../components/Container';
 import { Hero, HeroIntro, HeroTitle, HeroTeaser } from '../containers/Hero';
@@ -7,32 +7,43 @@ import { LatestArticles } from '../containers/LatestArticles';
 import { Seo } from '../containers/Seo';
 
 export default function IndexPage({ data }) {
+  const [error, setError] = React.useState(null);
   useEffect(() => {
     document.oncontextmenu = function () {
       return false;
     };
   });
-  return (
-    <>
-      <Hero>
-        <Seo />
-        <HeroIntro>Hi, my name is</HeroIntro>
-        <HeroTitle>
-          <strong>Piyush Mehta</strong>
-          <br />I make the web great.
-        </HeroTitle>
-        <HeroTeaser>
-          I am a Full Stack Developer/Designer based in India. I create, Design
-          tools for web and teach how to build high quality websites and
-          applications using JavaScript, React
-        </HeroTeaser>
-      </Hero>
-      <Container forwardedAs="section" pb={5}>
-        <SectionTitle forwardedAs="h2">Blog</SectionTitle>
-        <LatestArticles edges={data.allMdx.edges} />
-      </Container>
-    </>
-  );
+  try {
+    return (
+      <>
+        <Hero>
+          <Seo />
+          <HeroIntro>Hi, my name is</HeroIntro>
+          <HeroTitle>
+            <strong>Piyush Mehta</strong>
+            <br />I make the web great.
+          </HeroTitle>
+          <img src={data.photo.childImageSharp}></img>
+          <HeroTeaser>
+            I am a Full Stack Developer/Designer based in India. I create,
+            Design tools for web and teach how to build high quality websites
+            and applications using JavaScript, React
+          </HeroTeaser>
+        </Hero>
+        <Container forwardedAs="section" pb={5}>
+          <SectionTitle forwardedAs="h2">Blog</SectionTitle>
+          <LatestArticles edges={data.allMdx.edges} />
+        </Container>
+      </>
+    );
+  } catch (error) {
+    Sentry.configureScope((scope) => {
+      Object.keys(errorInfo).forEach((key) => {
+        scope.setExtra(key, errorInfo[key]);
+      });
+    });
+    Sentry.captureException(error);
+  }
 }
 
 export const pageQuery = graphql`
@@ -54,6 +65,13 @@ export const pageQuery = graphql`
             slug
             date(formatString: "MMMM DD, YYYY")
           }
+        }
+      }
+    }
+    photo: file(relativePath: { eq: "piyush-devfest.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 1000) {
+          ...GatsbyImageSharpFluid_withWebp
         }
       }
     }
