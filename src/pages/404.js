a2fc57cde139852fbfee6396311d80db9b86940b
Piyush Mehta <me@piyushmehta.com>
2020-09-05 15:37:02 +0530

    various fixes, issues resolve and new blog

--- src/pages/404.js
+++ src/pages/404.js
@@ -31,7 +31,7 @@ export default function NotFound() {
           />
         </a>
       </SectionDescription>
-      <div></div>
+      <div />
     </PageContainer>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
-import React, { useEffect } from 'react';
-import { graphql, useStaticQuery } from 'gatsby';
+import React from 'react';
+import { graphql } from 'gatsby';
+import GitHubButton from 'react-github-btn';
 import { SectionTitle } from '../components/Section';
 import { Container } from '../components/Container';
 import { Hero, HeroIntro, HeroTitle, HeroTeaser } from '../containers/Hero';
@@ -7,60 +8,54 @@ import { LatestArticles } from '../containers/LatestArticles';
 import { Seo } from '../containers/Seo';
 
 export default class IndexPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { error: null };
-  }
-  componentDidCatch(error, errorInfo) {
-    this.setState({ error });
-    Sentry.configureScope((scope) => {
-      Object.keys(errorInfo).forEach((key) => {
-        scope.setExtra(key, errorInfo[key]);
-      });
-    });
-    Sentry.captureException(error);
-
-    console.log(
-      '%c' + 'Made By Piyush Mehta',
-      'font-family:Comic Sans MS; font-size:50px; font-weight:bold; background: linear-gradient(#f00, yellow); border-radius: 5px; padding: 20px'
-    );
-  }
   componentDidMount() {
     document.oncontextmenu = function () {
       return false;
     };
+    console.log(
+      '%c Made By Piyush Mehta',
+      'font-family:Comic Sans MS; font-size:50px; font-weight:bold; background: linear-gradient(#f00, yellow); border-radius: 5px; padding: 20px'
+    );
   }
+
   render() {
-    if (this.state.error) {
-      return <h1>Something went wrong!</h1>;
-    } else {
-      return (
-        <>
-          <Hero>
-            <Seo />
-            <HeroIntro>Hi, my name is</HeroIntro>
-            <HeroTitle>
-              <strong>Piyush Mehta</strong>
-              <br />I make the web great.
-            </HeroTitle>
-            <HeroTeaser>
-              I am a full-time Full Stack Developer/Designer, a hobbyist Virtual
-              Aviation Simmer with an experience of (180+ hours) and a trumpet
-              player based in India.
-              <br />
-              <br />
-              I create, design tools and Apps for companies and individuals.
-              <br />I write down my learnings in my blog sections so that people
-              can learn from my mistakes.
-            </HeroTeaser>
-          </Hero>
-          <Container forwardedAs="section" pb={5}>
-            <SectionTitle forwardedAs="h2">Blog</SectionTitle>
-            <LatestArticles edges={this.props.data.allMdx.edges} />
-          </Container>
-        </>
-      );
-    }
+    return (
+      <>
+        <Hero>
+          <Seo />
+          <HeroIntro>Hi, my name is</HeroIntro>
+          <HeroTitle>
+            <strong>Piyush Mehta</strong>
+            <br />
+          </HeroTitle>
+
+          <HeroTeaser>
+            I am a full-time Full Stack Developer/Designer, a hobbyist Virtual
+            Aviation Simmer with an experience of (180+ hours) and a trumpet
+            player based in India.
+            <br />
+            <br />
+            I create, design tools and Apps for companies and individuals.
+            <br />
+            <br />I write down my learnings in my blog sections so that people
+            can learn from my mistakes.
+          </HeroTeaser>
+          <GitHubButton
+            href="https://github.com/piyush97"
+            data-color-scheme="no-preference: dark; light: light; dark: light;"
+            data-size="large"
+            data-show-count="true"
+            aria-label="Follow @piyush97 on GitHub"
+          >
+            Follow @piyush97
+          </GitHubButton>
+        </Hero>
+        <Container forwardedAs="section" pb={5}>
+          <SectionTitle forwardedAs="h2">Blog</SectionTitle>
+          <LatestArticles edges={this.props.data.allMdx.edges} />
+        </Container>
+      </>
+    );
   }
 }
 
