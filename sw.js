2b3dcd6c0c73688fedf1add24c96d4e94c9b6ce0
Travis CI User <travis@example.org>
2020-09-25 09:59:44 +0000

    Updates

--- sw.js
+++ sw.js
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "98c6bb5527e77b031b8b5a2a36cf6444"
+    "revision": "2453c868f94fb0a1ab5278df715ac3dd"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-e5cb9e0c77a823b86dc2.js"
