3727f7db47d7090267569f665cc28a21c3244264
Piyush Mehta <me@piyushmehta.com>
2020-09-28 00:41:43 +0530

    added rss feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,7 @@ const TitleName = 'Piyush Mehta';
 module.exports = {
   siteMetadata: {
     title: TitleName,
+    description: 'Piyush Mehta Full Stack React Developer',
     siteUrl,
     canonicalUrl: siteUrl,
     keywords: [
@@ -64,6 +65,60 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMdx } }) => {
+              return allMdx.edges.map((edge) => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  date: edge.node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  custom_elements: [{ 'content:encoded': edge.node.html }],
+                });
+              });
+            },
+            query: `
+            {
+              allMdx(sort: {order: DESC, fields: [frontmatter___date]}) {
+                edges {
+                  node {
+                    excerpt
+                    html
+                    fields {
+                      slug
+                    }
+                    frontmatter {
+                      title
+                      date
+                    }
+                  }
+                }
+              }
+            }
+            `,
+            output: '/rss.xml',
+            title: "Piyush Mehta Site's RSS Feed",
+            match: '^/blog/',
+          },
+        ],
+      },
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
