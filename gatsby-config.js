fb56472588b52de5248d2bdb5abe11c26aac5e1d
Piyush Mehta <me@piyushmehta.com>
2020-09-28 00:50:38 +0530

    links in  rss fixed

--- gatsby-config.js
+++ gatsby-config.js
@@ -87,7 +87,10 @@ module.exports = {
                 return Object.assign({}, edge.node.frontmatter, {
                   description: edge.node.excerpt,
                   date: edge.node.frontmatter.date,
-                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  url:
+                    site.siteMetadata.siteUrl +
+                    '/blog/' +
+                    edge.node.fields.slug,
                   guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
                   custom_elements: [{ 'content:encoded': edge.node.html }],
                 });
