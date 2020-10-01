91668f38b508fe8c6ceb3bc1e119a753fda7355e
mihirgupta0900 <mihirgupta0900@gmail.com>
2020-10-02 02:09:58 +0530

    added more styling

--- src/components/Tags.js
+++ src/components/Tags.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import styled from '@xstyled/styled-components';
 
-const InlineH3 = styled.span`
-  font-size: 1.17em;
+const TagP = styled.p`
+  background-color: light500;
+  border-radius: 5px;
+  padding: 0.2em 0.4em 0.2em 0.4em;
+  font-size: 0.9em;
+  margin: 0.4em;
+`;
+
+const TagDiv = styled.div`
+  display: flex;
+  flex-wrap: wrap;
 `;
 
 const Tags = ({ categories }) => {
   return (
     <div>
-      <InlineH3>Tags: </InlineH3>
-      <span>{categories[0]}</span>
-      {categories.map((cate, i) => {
-        if (i === 0) {
-          return null;
-        }
-        return <span>, {cate}</span>;
-      })}
+      <h3>Tags: </h3>
+      <TagDiv>
+        {categories.map((cate) => {
+          return <TagP>{cate}</TagP>;
+        })}
+      </TagDiv>
     </div>
   );
 };
