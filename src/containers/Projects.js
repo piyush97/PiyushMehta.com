77f2997d8f13d76c32b95d4e7b710f5416f304bb
Piyush Mehta <me@piyushmehta.com>
2021-02-27 21:30:53 +0530

    https

--- src/containers/Projects.js
+++ src/containers/Projects.js
@@ -409,7 +409,7 @@ function Projects({ projects }) {
       tags={['Ionic', 'Sass', 'React']}
     />,
     <ProjectTemplate
-      logo="http://assets.stickpng.com/images/580b57fcd9996e24bc43c521.png"
+      logo="https://assets.stickpng.com/images/580b57fcd9996e24bc43c521.png"
       label={projects.Quickstagram.label}
       title="Quickstagram - React Instagram Clone"
       github="https://github.com/piyush97/quickstagram"
