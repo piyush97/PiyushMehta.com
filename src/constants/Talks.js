1bd726d875ae4cf6ddd2069c2531508adcc35ea2
Piyush Mehta <me@piyushmehta.com>
2021-08-03 17:55:59 +0530

    fixing

---
+++ src/constants/Talks.js
@@ -0,0 +1,9 @@
+export const TalksData = [
+  {
+    id: 1,
+    topic: 'Introduction to Docker & Kubernetes',
+    photo: 'photo.jpg',
+    venue: 'Ramaiah Institute of Technology',
+    date: new Date(),
+  },
+];
