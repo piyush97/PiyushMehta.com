8544667cb5d159f7eac6b548081370a3c54323bc
Piyush Mehta <me@piyushmehta.com>
2021-08-03 23:45:34 +0530

    Contact Me --[WIP]

--- src/containers/WorkshopsContainer.js
+++ src/containers/WorkshopsContainer.js
@@ -1,20 +1,11 @@
 import { Box } from '@xstyled/styled-components';
 import React from 'react';
-import { Card, CardBody, CardTitle } from '../components/Card';
 import { TalksData } from '../constants/Talks';
 
 const WorkshopsContainer = () => {
   return (
     <Box>
-      {TalksData.map(({ id, venue, topic, photo, date }) => (
-        <Card key={id}>
-          <CardBody>
-            <CardTitle>{topic}</CardTitle>
-            <Img fluid={photo} draggable={false} />
-            {venue}
-          </CardBody>
-        </Card>
-      ))}
+      <TalksData />
     </Box>
   );
 };
