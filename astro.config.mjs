8b4a4c966aab5d4cd730b8a222c10eec804418ec
piyush97 <me@piyushmehta.com>
2022-08-29 17:19:01 -0400

    added deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), svelte()]
+});
