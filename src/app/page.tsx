f54fca233be80d449c20898fa261859ce33c70e5
piyush97 <me@piyushmehta.com>
2024-04-24 23:41:39 -0400

    refactor: Update Hero component and global CSS
    - Updated Hero component with content and buttons for View Projects and Contact Me
    - Removed unnecessary CSS variables from global CSS
    - Modified base and dark mode styles in global CSS
    - Optimized Hero image for better performance

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import Hero from "@/components/custom/Hero/hero";
+
 export default function Home() {
-  return <main className=""></main>;
+  return (
+    <main className="">
+      <Hero />
+    </main>
+  );
 }
