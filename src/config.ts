d0ad997b2eb70867574914d8ea2c559f70662c46
piyush97 <me@piyushmehta.com>
2024-05-06 22:41:22 -0400

    feat: Add Canadian French locale messages
    This commit adds the Canadian French locale messages to the project. The messages are added to the `messages/ca-fr.json` file. The `AboutMe` section is translated to French.

--- src/config.ts
+++ src/config.ts
@@ -6,13 +6,13 @@ export const host = process.env.VERCEL_URL
   : `http://localhost:${port}`;
 
 export const defaultLocale = "en" as const;
-export const locales = ["en", "fr"] as const;
+export const locales = ["en", "ca-fr"] as const;
 
 export const pathnames = {
   "/": "/",
   "/pathnames": {
     en: "/pathnames",
-    fr: "/pfadnamen",
+    "ca-fr": "/pathnames-fr",
   },
 } satisfies Pathnames<typeof locales>;
 
